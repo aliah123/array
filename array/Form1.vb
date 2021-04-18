f9ed149244309d644c81c8ffb68284c896c63e5a
Nur Aliah Husna Binti Mohamed Husaini <18DDT19F1098@student.ptss.edu.my>
2021-04-18 14:39:38 +0800

    Add project files.

---
+++ array/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strNamaHari(6) As String
+        Dim nilai As Integer
+        strNamaHari(0) = "Ahad"
+        strNamaHari(1) = "Isnin"
+        strNamaHari(2) = "Selasa"
+        strNamaHari(3) = "Rabu"
+        strNamaHari(4) = "Khamis"
+        strNamaHari(5) = "Jumaat"
+        strNamaHari(6) = "Sabtu"
+
+        System.Array.Reverse(strNamaHari)
+        For x = 0 To 6
+            ListName.Items.Add(strNamaHari(x))
+        Next
+
+    End Sub
+End Class
